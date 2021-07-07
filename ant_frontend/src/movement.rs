0ffe93ca9dafd9f37182be12b80097140dc232b5
Abhinav Chavali <abhinavchavali12@gmail.com>
2021-07-07 20:17:06 +0000

    Fixed #7

--- ant_frontend/src/movement.rs
+++ ant_frontend/src/movement.rs
@@ -18,16 +18,35 @@ pub fn move_down(screen: &mut impl Write, buffer: &mut DefaultBuffer) {
         } else if possible_width==0 {
             buffer.set_position(screen, 5, buffer.current_y+1);
             screen.flush().unwrap();
-        } 
+        } else if possible_width<width {
+            buffer.set_position(screen, (possible_width+4) as u16, buffer.current_y+1);
+
+            screen.flush().unwrap();
+        }
+
     }
     
 }
 
 pub fn move_up(screen: &mut impl Write, buffer: &mut DefaultBuffer) {
     if buffer.current_y!=1 {
-        buffer.set_position(screen, buffer.current_x, buffer.current_y-1);
+        let height = buffer.current_y as usize;
+        let width = buffer.current_x as usize;
         
-        screen.flush().unwrap();
+        let possible_width = buffer.lines[height-2].chars().count();
+        if possible_width+4>=width {
+            buffer.set_position(screen, buffer.current_x, buffer.current_y-1);
+            
+            screen.flush().unwrap();
+        } else if possible_width==0 {
+            buffer.set_position(screen, 5, buffer.current_y-1);
+
+            screen.flush().unwrap();
+        } else if possible_width<width {
+            buffer.set_position(screen, (possible_width+5) as u16, buffer.current_y-1);
+
+            screen.flush().unwrap();
+        }
 
     }
 }
