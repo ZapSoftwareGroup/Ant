3aa31aaa728259091fc7f1e469db13d750fac857
Abhinav Chavali <abhinavchavali12@gmail.com>
2021-07-07 19:36:17 +0000

    Added up boundary

--- ant_frontend/src/movement.rs
+++ ant_frontend/src/movement.rs
@@ -4,10 +4,23 @@ use crate::buffer::DefaultBuffer;
 
 pub fn move_down(screen: &mut impl Write, buffer: &mut DefaultBuffer) {
     if buffer.current_y!=buffer.line_count as u16 {
-        buffer.set_position(screen, buffer.current_x, buffer.current_y+1);
 
-        screen.flush().unwrap();
+        let height = buffer.current_y as usize;
+        let width = buffer.current_x as usize;
+        
+        let possible_width = buffer.lines[height].chars().count();
+
+        if !(width>=possible_width+5) {
+
+            buffer.set_position(screen, buffer.current_x, buffer.current_y+1);
+
+            screen.flush().unwrap();
+        } else if possible_width==0 {
+            buffer.set_position(screen, 5, buffer.current_y+1);
+            screen.flush().unwrap();
+        } 
     }
+    
 }
 
 pub fn move_up(screen: &mut impl Write, buffer: &mut DefaultBuffer) {
