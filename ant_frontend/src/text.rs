87386837985edc99b001bde2d71d4f1d22d0b95d
Abhinav Chavali <abhinavchavali12@gmail.com>
2021-07-16 14:27:42 +0000

    Allow Return to wrap text to new line

--- ant_frontend/src/text.rs
+++ ant_frontend/src/text.rs
@@ -7,9 +7,22 @@ use crate::movement::*;
 
 pub fn insert_newline(screen: &mut impl Write, buffer: &mut DefaultBuffer) {
     let current_line: usize = (buffer.current_y+buffer.shown_first-1) as usize;
-    buffer.lines.insert(current_line, (current_line, "".to_string()));
-    buffer.line_count+=1;
-    buffer.first_char = find_first_char(buffer.line_count);
+    let current_position = buffer.current_x-buffer.first_char;
+    let possible_width = buffer.lines[buffer.shown_first as usize+buffer.current_y as usize-2].1.chars().count();
+
+    if current_position == possible_width as u16 {
+        buffer.lines.insert(current_line, (current_line, "".to_string()));
+        buffer.line_count+=1;
+        buffer.first_char = find_first_char(buffer.line_count);
+    } else {
+        // delete original end of line
+        let substring = buffer.lines[current_line-1].1[current_position as usize..possible_width].to_string();
+        buffer.lines[current_line-1].1.replace_range(current_position as usize..possible_width, "");
+        buffer.lines.insert(current_line, (current_line, substring));
+        buffer.line_count+=1;
+        buffer.first_char = find_first_char(buffer.line_count);
+    }
+
 
     // Add 1 to line number of all lines after newly inserted one
     let mut counter = 0;
@@ -50,10 +63,10 @@ pub fn insert_char_at_pos(screen: &mut impl Write, buffer: &mut DefaultBuffer, c
 pub fn delete_char_or_newline(screen: &mut impl Write, buffer: &mut DefaultBuffer) {
 
     let current_line = (buffer.current_y+buffer.shown_first-1) as isize;
-    let (_line_num, text) = &buffer.lines[current_line as usize-1];
+    let (_line_num, text) = &buffer.lines[current_line as usize-2];
 
     if (buffer.current_x==buffer.first_char)&(text==&"".to_string())&(buffer.current_y!=1) {
-        let current_line = (buffer.current_y+buffer.shown_first-2) as usize;
+        let current_line = (buffer.current_y+buffer.shown_first-3) as usize;
         buffer.lines.remove(current_line);
         buffer.line_count -= 1;
         buffer.first_char = find_first_char(buffer.line_count);
